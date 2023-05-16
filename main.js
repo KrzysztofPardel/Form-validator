22d9345de2e94bdc6608df45976fde1f440f122a
KrzysztofPardel <pardelkrzysztof97@gmail.com>
2023-05-16 15:12:57 +0200

    JS finished

--- main.js
+++ main.js
@@ -45,7 +45,35 @@ const checkPassword = (pass1, pass2) => {
 	}
 };
 
+const checkMail = (email) => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	if (re.test(email.value)) {
+		clearError(email);
+	} else {
+		showError(email, 'Incorrect email');
+	}
+};
+
+const checkErrors = () => {
+	//check if the form can be sent out
+	const allInputs = document.querySelectorAll('.form-box');
+	let errorCount = 0;
+
+	allInputs.forEach((element) => {
+		if (element.classList.contains('error')) {
+			errorCount++;
+		}
+	});
+	if (errorCount === 0) {
+		popup.classList.add('show-popup');
+	}
+	console.log(errorCount);
+};
+
 const sendForm = (e) => {
+	//checking input values correctness prior to sending the form
 	e.preventDefault();
 
 	checkForm([username, password, repeatPassword, email]);
@@ -53,12 +81,15 @@ const sendForm = (e) => {
 	checkLength(password, 8);
 	checkLength(repeatPassword, 8);
 	checkPassword(password, repeatPassword);
+	checkMail(email);
+	checkErrors();
 };
 //---the below part works
 const clearForm = (e) => {
 	e.preventDefault();
 	[username, password, repeatPassword, email].forEach((element) => {
 		element.value = '';
+		clearError(element);
 	});
 };
 sendBtn.addEventListener('click', sendForm);
